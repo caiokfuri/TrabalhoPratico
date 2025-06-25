f6d0c882b7efe0a3c2a23635cfae2127ab35866f
Mateus Ribas <marcalmateus315@gmail.com>
2025-06-25 14:33:52 -0300

    Implementaçao de testes automatizados com munit (Crud Veiculos)

---
+++ Tests/testesVeiculo.cpp
@@ -0,0 +1,34 @@
+#include "../munit/munit.h"
+#include "../Models/Veiculo.h"
+#include <cstring>
+extern "C" {
+    #include "../Munit/munit.h"
+}
+
+static MunitResult test_set_e_get_status(const MunitParameter[], void*) {
+    Veiculo v;
+    v.setStatus(1);
+    munit_assert_int(v.getStatus(), ==, 1);
+    return MUNIT_OK;
+}
+
+static MunitResult test_set_e_get_localAtual(const MunitParameter[], void*) {
+    Veiculo v;
+    v.setLocalAtual("Garagem");
+    munit_assert_string_equal(v.getLocalAtual(), "Garagem");
+    return MUNIT_OK;
+}
+
+static MunitTest tests[] = {
+    {(char*)"/veiculo/status", test_set_e_get_status, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {(char*)"/veiculo/localAtual", test_set_e_get_localAtual, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
+};
+
+static const MunitSuite suite = {
+    (char*)"/slem-tests", tests, NULL, 1, MUNIT_SUITE_OPTION_NONE
+};
+
+int main(int argc, char* argv[]) {
+    return munit_suite_main(&suite, NULL, argc, argv);
+}
